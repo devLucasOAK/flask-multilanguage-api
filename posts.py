9871b7a22629ed8be90c5c379b2b8f9629274d6c
devLucasOAK <devlucasoak@outook.com>
2021-10-11 02:18:20 -0300

    Real Data

--- posts.py
+++ posts.py
@@ -2,44 +2,88 @@ posts = [
     {
         'id': 1,
         'quantity_left': 3,
-        'image': 'dummyimage.com/300&text=Object_1',
+        'image': 'https://dummyimage.com/300&text=Florida',
         'translations': [
             {
                 'locale': 'en',
-                'title': 'First Title in English',
-                'description': 'First Description in English',
+                'title': 'Florida Hotel',
+                'description': 'Located at the Florida Mall and close to the Orlando Airport, The Florida Hotel and Conference Center offers city convenience combined with upscale comfort and warm hospitality. From our friendly atmosphere with genuine personalized service, outstanding hotel amenities including casual on site dining, 24 hour fitness center and business center, open air swimming pool. Combined with our unbeatable central location, you’ll discover a welcome and comfortable home base for an unforgettable Orlando vacation in the sunshine state.',
             },
             {
                 'locale': 'de',
-                'title': 'Frst Title in German',
-                'description': 'First Description in German',
+                'title': 'Florida Hotel',
+                'description': 'Das The Florida Hotel and Conference Center liegt an der Florida Mall und in der Nähe des Flughafens Orlando und bietet städtische Annehmlichkeiten kombiniert mit gehobenem Komfort und herzlicher Gastfreundschaft. Von unserer freundlichen Atmosphäre mit echtem persönlichem Service, herausragenden Hotelannehmlichkeiten, einschließlich zwanglosem Essen vor Ort, 24-Stunden-Fitnesscenter und Businesscenter, Open-Air-Swimmingpool. In Kombination mit unserer unschlagbaren zentralen Lage entdecken Sie eine willkommene und komfortable Ausgangsbasis für einen unvergesslichen Urlaub in Orlando im Sonnenscheinstaat.',
             },
             {
                 'locale': 'ar',
-                'title': 'Frst Title in Arabic',
-                'description': 'First Description in Arabic'
+                'title': 'فندق فلوريدا',
+                'description': 'يقع فندق ومركز مؤتمرات فلوريدا في فلوريدا مول وبالقرب من مطار أورلاندو ، ويوفر راحة المدينة جنبًا إلى جنب مع الراحة الراقية وكرم الضيافة. من أجواء ودية مع خدمة شخصية حقيقية ، ووسائل راحة فندقية رائعة بما في ذلك تناول طعام غير رسمي في الموقع ، ومركز لياقة بدنية على مدار 24 ساعة ومركز أعمال ، وحمام سباحة في الهواء الطلق. بالاقتران مع موقعنا المركزي الذي لا يهزم ، ستكتشف قاعدة منزلية مريحة ومرحبة لقضاء عطلة لا تُنسى في أورلاندو في حالة أشعة الشمس.'
             }
         ],
     },
     {
         'id': 2,
-        'quantity_left': 2,
-        'image': 'dummyimage.com/300&text=Object_2',
+        'quantity_left': 5,
+        'image': 'https://dummyimage.com/300&text=Austin',
         'translations': [
             {
                 'locale': 'en',
-                'title': 'Second Title in English',
-                'description': 'Second Description in English',
+                'title': 'Austin Southpark Hotel',
+                'description': 'With a premier location mid way between Austin-Bergstrom International Airport and Austin’s center city, the Austin Southpark Hotel is at the center of it all. Rising 14 stories above Austin’s flourishing high-tech corridor, this elegant luxury hotel offers all modern amenities for the 21 century traveler. Relax in the comfort of your deluxe accommodation, savor find cuisine either in our famous Onion Creek Grill Restaurant or indulge yourself in the privacy of your room with in-room dining. Discover the excitement of downtown Austin, the State Capitol, and the Sixth Street Entertainment District, all just moments away. The Austin Southpark Hotel is the ideal destination for business and pleasure.',
             },
             {
                 'locale': 'de',
-                'title': 'Second Title in German',
-                'description': 'Second Description in German',
+                'title': 'Austin Southpark Hotel',
+                'description': 'Mit einer erstklassigen Lage auf halbem Weg zwischen dem internationalen Flughafen Austin-Bergstrom und der Innenstadt von Austin liegt das Austin Southpark Hotel im Zentrum von allem. Dieses elegante Luxushotel erhebt sich 14 Stockwerke über dem blühenden High-Tech-Korridor von Austin und bietet alle modernen Annehmlichkeiten für den Reisenden des 21. Jahrhunderts. Entspannen Sie sich im Komfort Ihrer Deluxe-Unterkunft, genießen Sie Gerichte in unserem berühmten Onion Creek Grill Restaurant oder lassen Sie sich in der Privatsphäre Ihres Zimmers mit Speisen auf dem Zimmer verwöhnen. Entdecken Sie die aufregende Innenstadt von Austin, das State Capitol und das Unterhaltungsviertel Sixth Street, alles nur wenige Schritte entfernt. Das Austin Southpark Hotel ist das ideale Ziel für Geschäfts- und Urlaubsreisende.',
             },
             {
                 'locale': 'ar',
-                'title': 'Second Title in Arabic',
-                'description': 'Second Description in Arabic',
+                'title': 'فندق أوستن ساوثبارك ',
+                'description': 'مع موقع متميز في منتصف الطريق بين مطار أوستن بيرجستروم الدولي ووسط مدينة أوستن ، يقع فندق Austin Southpark في مركز كل ذلك. يرتفع هذا الفندق الفاخر والأنيق 14 طابقًا فوق ممر أوستن المزدهر عالي التقنية ، ويوفر جميع وسائل الراحة الحديثة للمسافر في القرن الحادي والعشرين. تمتع بالاسترخاء في راحة الإقامة الفاخرة الخاصة بك ، وتذوق المأكولات في مطعم Onion Creek Grill الشهير أو انغمس في خصوصية غرفتك مع تناول الطعام في الغرفة. اكتشف الإثارة في وسط مدينة أوستن ، ومبنى الكابيتول ، ومنطقة الترفيه في الشارع السادس ، كل ذلك على بعد لحظات فقط. فندق أوستن ساوثبارك هو الوجهة المثالية للعمل والمتعة.',
+            }
+        ],
+    },
+    {
+        'id': 3,
+        'quantity_left': 10,
+        'image': 'https://dummyimage.com/300&text=Cleveland',
+        'translations': [
+            {
+                'locale': 'en',
+                'title': 'Drury Plaza Hotel Cleveland Downtown',
+                'description': 'Completely renovated, the Drury Plaza Hotel Cleveland Downtown is the former Board of Education building. The 189-room hotel sits downtown and boasts more than 3,900 square feet of meeting space. The two-story lobby features two fully restored murals painted by Cora Holden in 1931. Drury Hotels began the restoration and preservation of the building in 2014, which is within walking distance of FirstEnergy Stadium (Cleveland Browns), Rock and Roll Hall of Fame and Museum, the Cleveland Indians’ Progressive Field and the Quicken Loans Arena.',
+            },
+            {
+                'locale': 'de',
+                'title': 'Drury Plaza Hotel Cleveland Downtown',
+                'description': 'Das komplett renovierte Drury Plaza Hotel Cleveland Downtown ist das ehemalige Board of Education-Gebäude. Das Hotel mit 189 Zimmern liegt in der Innenstadt und verfügt über mehr als 3.900 Quadratmeter Tagungsfläche. Die zweistöckige Lobby verfügt über zwei vollständig restaurierte Wandgemälde, die 1931 von Cora Holden gemalt wurden. Drury Hotels begannen 2014 mit der Restaurierung und Erhaltung des Gebäudes, das nur wenige Gehminuten vom FirstEnergy Stadium (Cleveland Browns), der Rock and Roll Hall of Fame und Museum, das Progressive Field der Cleveland Indians und die Quicken Loans Arena.',
+            },
+            {
+                'locale': 'ar',
+                'title': 'فندق دروري بلازا كليفلاند داون تاون',
+                'description': 'تم تجديد Drury Plaza Hotel Cleveland Downtown بالكامل ، وهو مبنى مجلس التعليم السابق. يقع الفندق المؤلف من 189 غرفة في وسط المدينة ويضم أكثر من 3900 قدم مربع من مساحة الاجتماعات. يضم اللوبي المكون من طابقين جداريتين تم ترميمهما بالكامل بواسطة Cora Holden في عام 1931. بدأت Drury Hotels في ترميم المبنى والحفاظ عليه في عام 2014 ، والذي يقع على مسافة قريبة من ملعب FirstEnergy Stadium (Cleveland Browns) و Rock and Roll Hall of Fame و المتحف ، وحقل كليفلاند التقدمي للهنود وساحة كويكن لوانز.',
+            }
+        ],
+    },
+    {
+        'id': 4,
+        'quantity_left': 20,
+        'image': 'https://dummyimage.com/300&text=Washington',
+        'translations': [
+            {
+                'locale': 'en',
+                'title': 'The Washington Plaza',
+                'description': 'Discover the modern elegance of the newly-renovated Washington Plaza. A balance of cosmopolitan sophistication and casual grace, our resort-like hotel offers spacious, boutique accommodations in an ideal location setting. Located downtown on Thomas Circle, our hotel sits in the city´s trendiest neighborhood, and is only minutes from the iconic attractions of the Nation´s Capital. Lounge by our beautifully-landscaped pool, a design highlight of the hotel´s world-famous architect, Morris Lapidus. Enjoy innovative American fare dining at 10 Thomas Restaurant, a favorite of both guests and discerning DC residents, or plan your next business meeting or wedding in our 12,000 sq. ft. of flexible meeting space, immersed in the stylish ambiance of our boutique hotel.',
+            },
+            {
+                'locale': 'de',
+                'title': 'The Washington Plaza',
+                'description': 'Das komplett renovierte Drury Plaza Hotel Cleveland Downtown ist das ehemalige Board of Education-Gebäude. Das Hotel mit 189 Zimmern liegt in der Innenstadt und verfügt über mehr als 3.900 Quadratmeter Tagungsfläche. Die zweistöckige Lobby verfügt über zwei vollständig restaurierte Wandgemälde, die 1931 von Cora Holden gemalt wurden. Drury Hotels begannen 2014 mit der Restaurierung und Erhaltung des Gebäudes, das nur wenige Gehminuten vom FirstEnergy Stadium (Cleveland Browns), der Rock and Roll Hall of Fame und Museum, das Progressive Field der Cleveland Indians und die Quicken Loans Arena.',
+            },
+            {
+                'locale': 'ar',
+                'title': 'واشنطن بلازا',
+                'description': 'اكتشف الأناقة العصرية لواشنطن بلازا المجددة حديثًا. توازن بين الرقي العالمي والأناقة غير الرسمية ، يوفر فندقنا الشبيه بالمنتجع أماكن إقامة بوتيكية واسعة في موقع مثالي. يقع فندقنا في وسط المدينة في Thomas Circle ، في أكثر الأحياء العصرية في المدينة ، وعلى بعد دقائق فقط من مناطق الجذب الشهيرة في Nation´s Capital. استرخ بجانب المسبح ذو المناظر الطبيعية الجميلة ، وهو أحد أبرز معالم تصميم المهندس المعماري الشهير للفندق ، موريس لابيدوس. استمتع بتناول طعام أمريكي مبتكر في مطعم 10 Thomas ، المفضل لكل من الضيوف والمقيمين المميزين في العاصمة ، أو خطط لاجتماع عملك أو حفل زفافك القادم في مساحة اجتماعات مرنة تبلغ 12000 قدم مربع ، مغمورة في الأجواء الأنيقة لفندقنا البوتيك.',
             }
         ],
     }
